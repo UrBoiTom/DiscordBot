d33bf40208b88dc7b1db8067c22091baee60a826
dragoman44 <58535804+dragoman44@users.noreply.github.com>
2025-05-18 20:47:40 -0400

    Add files via upload
    Linux friendly version of files.

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git pull --rebase --autostash
+
+if [ ! -d "bot-env" ]; then
+  python3 -m venv bot-env
+fi
+
+source bot-env/bin/activate
+pip install -U -r requirements.txt
+python3 bots.py
