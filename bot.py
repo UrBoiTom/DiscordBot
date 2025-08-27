a3a01e4dfdd6c53850950d7b24a7d29d196148d2
Sorro123 <blackassassin2416@gmail.com>
2025-08-27 21:39:42 +0200

    Hotfix for future bugs

--- bot.py
+++ bot.py
@@ -4,37 +4,41 @@ import os
 import sys
 import scripts.functions as functions
 
-name = sys.argv[1]
+def main():
+    name = sys.argv[1]
 
-keys = functions.load_json('Variables/keys')
+    keys = functions.load_json('Variables/keys')
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.members = True
 
-contexts = discord.app_commands.AppCommandContext(guild=True, dm_channel=True, private_channel=True)
+    contexts = discord.app_commands.AppCommandContext(guild=True, dm_channel=True, private_channel=True)
 
-client = commands.Bot(command_prefix='/', intents=intents, allowed_contexts=contexts)
-client.main_name = name
+    client = commands.Bot(command_prefix='/', intents=intents, allowed_contexts=contexts)
+    client.main_name = name
 
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user}')
+    @client.event
+    async def on_ready():
+        print(f'Logged in as {client.user}')
 
-    for cog in os.listdir('cogs'):
-        if cog.endswith('.py'):
-            try:
-                await client.load_extension(f'cogs.{cog[:-3]}')
-                print (f"Loaded {cog}")
-            except Exception as e:
-                print(f'Failed to load extension {cog}: {e}')
-    
-    try:
-        synced = await client.tree.sync()
-        if (len(synced) == 1): plural = ""
-        else: plural = 's'
-        print(f"Synced {len(synced)} command{plural}")
-    except Exception as e:
-        print(f"Failed to sync commands: {e}")
+        for cog in os.listdir('cogs'):
+            if cog.endswith('.py'):
+                try:
+                    await client.load_extension(f'cogs.{cog[:-3]}')
+                    print (f"Loaded {cog}")
+                except Exception as e:
+                    print(f'Failed to load extension {cog}: {e}')
+        
+        try:
+            synced = await client.tree.sync()
+            if (len(synced) == 1): plural = ""
+            else: plural = 's'
+            print(f"Synced {len(synced)} command{plural}")
+        except Exception as e:
+            print(f"Failed to sync commands: {e}")
 
-client.run(keys[name]["client_key"])
+    client.run(keys[name]["client_key"])
+
+if __name__ == '__main__':
+    main()
